ca9d093b2d6f3795b82c819e52b194ca517387d2
brightonmboya <b.mboya@alustudent.com>
2025-12-09 15:51:21 +0300

    fix better auth middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getSessionCookie } from 'better-auth/cookies'
+import { NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-    const sessionCookie = getSessionCookie(request)
-    const { pathname } = request.nextUrl
+// import { getSessionCookie } from 'better-auth/cookies'
 
-    if (sessionCookie && ['/sign-up', '/login'].includes(pathname)) {
-        return NextResponse.redirect(new URL('/cms', request.url))
-    }
-
-    if (!sessionCookie && pathname.startsWith('/cms')) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
+export async function middleware() {
+    // const sessionCookie = getSessionCookie(request)
+    // const { pathname } = request.nextUrl
+    //
+    // if (sessionCookie && ['/sign-up', '/login'].includes(pathname)) {
+    //     return NextResponse.redirect(new URL('/cms', request.url))
+    // }
+    //
+    // if (!sessionCookie && pathname.startsWith('/cms')) {
+    //     return NextResponse.redirect(new URL('/login', request.url))
+    // }
 
     return NextResponse.next()
 }
