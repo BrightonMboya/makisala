066177871d739987c7c3124231c70ed708466760
brightonmboya <b.mboya@alustudent.com>
2025-12-21 17:15:04 +0300

    sitemap build fix

--- apps/makisala/src/app/to_book/sitemap.ts
+++ apps/makisala/src/app/to_book/sitemap.ts
@@ -2,11 +2,7 @@ import type { MetadataRoute } from 'next'
 import { db, tourPackages } from '@/db'
 import { BASE_URL } from '@/lib/constants'
 
-export default async function generateSitemaps({
-    id,
-}: {
-    id: number
-}): Promise<MetadataRoute.Sitemap> {
+export default async function generateSitemaps(): Promise<MetadataRoute.Sitemap> {
     // Google's limit is 50,000 URLs per sitemap
     const tours_slugs = await db
         .select({ slug: tourPackages.slug, updatedAt: tourPackages.updatedAt })
@@ -14,7 +10,6 @@ export default async function generateSitemaps({
 
     return tours_slugs.map(tour => ({
         url: `${BASE_URL}/to_book/${tour.slug}`,
-        lastModified: new Date('2025-09-27').toISOString(),
-        // lastModified: new Date(tour.updatedAt).toISOString()
+        lastModified: new Date(tour.updatedAt).toISOString(),
     }))
 }
